4e740cfeae743e1dd0d5d6ad0afb69d3c16833fa
Alan Cano Sanchez <cs4lan01@outlook.com>
2023-09-17 16:01:38 -0500

    Se agrego la codificacion y empaquetamiento

--- teoria_comunicacion.py
+++ teoria_comunicacion.py
@@ -1,97 +1,125 @@
 import requests
+import numpy as np
 from PIL import Image
 from io import BytesIO
 import matplotlib.pyplot as plt
-import numpy as np
+import time
+import random
 
 def fuente_informacion():
-
     print("Obteniendo la imagen....")
-
     api_url = 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-
-    # Hacer la solicitud a la URL de la imagen
     response = requests.get(api_url)
-
-    # Obtener el contenido de la respuesta (datos binarios)
     img_content = response.content
-
-    # Convertir los datos binarios a una imagen de PIL
     img = Image.open(BytesIO(img_content))
-
     return img
 
+def transmisor(img_original):
 
-def transmisor(img):
+    # Imagen a arreglo de numpy
+    img_array = np.array(img_original)
 
-  print("Convertir la imágen en un arreglo de píxeles para su envio....")
+    # Codificamos la imagen sumando 2 a cada valor
+    img_codificada = img_array + 2
+    print("Imágen codificada con exito!")
+    
+    # Obtener las dimensiones de la imagen
+    alto, ancho, canales = img_codificada.shape
 
-  # Convertir la imagen de PIL a un arreglo numpy de píxeles
-  img_array = np.array(img)
-
-  return img_array
-
-
-def canal(img_ruido):
+    # Tamaño de paquete (ejemplo)
+    tamano_paquete = 100
   
-  print("Imágen enviandose al receptor....")
 
-  return ruido(img_ruido)
+    # Empaquetamiento de imagen
+    img_empaq = []
+    header = [1, 2, 3, 4]  # Header 
+    tail = [5, 6, 7, 8]  # Tail
 
+    for i in range(0, alto * ancho * canales, tamano_paquete):
+        bloque = img_codificada.flat[i:i + tamano_paquete]
+        paquete = np.concatenate((header, bloque, tail))
+        img_empaq.append(paquete)
+    
 
-def ruido(img_ruido):
+    print("Imágen empaquetada con exito y lista para su envio!")
+    return img_empaq,alto,ancho, canales
 
-  print("Presencia de ruido en el canal!")
+def canal(paquetes):
+
+    print("Enviando paquetes por el canal...")
+    paquetes_recibidos = []
+    numeros_prob = list(range(1, 51))
+    umbrales = [11,12,13,14,15,41,42,43,44,45]
+
+    print("Presencia de ruido en el canal...")
+    for paquete in paquetes:
+        numero_aleatorio = random.choice(numeros_prob)
+        #print(numero_aleatorio)
+        if numero_aleatorio in umbrales:
+            #Simular ruido cambiando algunos valores aleatorios en el paquete
+            paquetes_recibidos.append(ruido(paquete))
+        else:
+            paquetes_recibidos.append(paquete)
+
+        time.sleep(.01)
+
+    return paquetes_recibidos
+
+def ruido(paquete):
 
   ruido = 20
 
-  ruido_array = np.random.randint(ruido, ruido + 1, size=img_ruido.shape, dtype=int)
+  #print("Presencia de ruido en el canal...")
 
-  return np.clip(img_ruido + ruido, 0, 255).astype(np.uint8)
- 
+  return np.clip(paquete + ruido, 0, 255).astype(np.uint8)
 
-def receptor(img_ruido):
+def receptor(paquetes_recibidos, alto, ancho, canales):
 
-  print("Imágen recibida por el receptor!")
+    print("Recibiendo paquetes y desempaquetando...")
+    paquetes_desempaquetados = []
 
-  img_final = Image.fromarray(img_ruido)
+    for paquete in paquetes_recibidos:
+        bloque = paquete[4:-4]  # Eliminar header y tail
+        paquetes_desempaquetados.append(bloque)
 
-  print("Imágen decodificada por el receptor!")
+    # Crear una matriz con los paquetes desempaquetados
+    img_desempaquetada = np.concatenate(paquetes_desempaquetados)
 
-  return img_final
+    # Decodificar la imagen restandole el 2, que sumamos en la codificacion 
+    img_decodificada = img_desempaquetada - 2
 
+    # Ajustar la forma de la matriz a las dimensiones originales
+    img_final = img_decodificada.reshape(alto, ancho, canales)
+
+    return img_final
 
 def destino(img_final):
 
-  print("Imágen llegada al destino!")
+  print("Imágen llegada a su destino con exito!")
 
-  return img_final
+  # Mostramos la imagen final
+  plt.imshow(img_final)
+  plt.title("Imagen Final")
+  plt.axis('off')
+  plt.show()
 
+# ---------------Codigo principal---------------------------
+img_original = fuente_informacion()
 
-# Codigo principal
-
-img = fuente_informacion()
-
-print("Imágen obtenida! -> ")
-
-plt.imshow(img)
-plt.axis('off') 
+plt.imshow(img_original)
+plt.title("Imagen Original")
+plt.axis('off')
 plt.show()
 
-img_codificada =  transmisor(img)
 
-print("Imágen codificada! ->", img_codificada[:2])
+# Empezamos el proceso de transmisión
+paquetes,alto,ancho,canales = transmisor(img_original)
 
-img_ruido = canal(img_codificada)
 
-print("Imágen alterada por ruido ->", img_ruido[:2])
+# Simulamos el canal de transmisión
+paquetes_recibidos = canal(paquetes)
 
-img_final = receptor(img_ruido)
+# Procedemos con la recepción y desempaquetado
+img_final = receptor(paquetes_recibidos, alto, ancho, canales)
 
-img_final = destino(img_final)
-
-print("Imágen final -> ")
-
-plt.imshow(img_final)
-plt.axis('off') 
-plt.show()
+destino(img_final)
